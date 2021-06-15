a8ab9b3cea295c16fe5098536d74494f343e270c
julian-hecker <julianheckerdev@gmail.com>
2021-06-15 17:28:45 -0400

    couple of changes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,19 @@
 import * as functions from "firebase-functions";
+import * as admin from 'firebase-admin';
+import * as express from 'express';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+
+
+app.get('/users', async (req, res) => {
+    try {
+        const users = await admin.firestore().collection('users').get();
+        return res.json(users);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send('Server Error, try again');
+    }
+});
+
+exports.api = functions.https.onRequest(app);
+// https://blog.logrocket.com/rest-api-firebase-cloud-functions-typescript-firestore/
